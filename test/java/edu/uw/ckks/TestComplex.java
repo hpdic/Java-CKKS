5544d092e5a0ec7897fae295bfb0a87ae95c6493
Dongfang Zhao <dzhao@uw.edu>
2023-08-26 19:42:35 +0000

    adding test

---
+++ test/java/edu/uw/ckks/TestComplex.java
@@ -0,0 +1,11 @@
+package test.java.edu.uw.ckks;
+
+import ckks.Complex;
+
+public class TestComplex {
+    
+    public static void main(String[] theArgs) {
+        Complex complex = new Complex(19, 81);
+        System.out.println(complex);
+    }
+}
